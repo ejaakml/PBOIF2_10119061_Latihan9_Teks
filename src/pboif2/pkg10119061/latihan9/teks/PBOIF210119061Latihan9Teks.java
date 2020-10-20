f3bd7a438b7981695a832b15db7f161d87944499
ejaakml <eximindotong@yahoo.com>
2020-10-20 18:07:59 +0700

    9

---
+++ src/pboif2/pkg10119061/latihan9/teks/PBOIF210119061Latihan9Teks.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan9.teks;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       char c;
+       char tab = '\t';
+       char newline = '\n';
+       c = 'A';
+       System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
